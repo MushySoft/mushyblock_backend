dc121d942aea8039cf5dc17c6c856fa89859706a
yurtsev <yurtsevburiy@mail.ru>
2025-02-21 14:35:37 +0300

    chore: restruct metro models BREAKING CHANGE: create associate table for Station and Line models, implemented many-to-many relationship

--- src/metro/models.py
+++ src/metro/models.py
@@ -1,20 +1,32 @@
-from src.database import Base
-from sqlalchemy import Column, String, Float, Integer, ForeignKey
+from src import Base
+from sqlalchemy import Table, Column, String, Float, Integer, ForeignKey
 from sqlalchemy.orm import relationship
+
+
+MetroMatch = Table(
+    'metro_match',
+    Base.metadata,
+    Column('line_id', Integer, ForeignKey('line.id')),
+    Column('station_id', Integer, ForeignKey('station.id'))
+)
 
 
 class Station(Base):
     __tablename__ = "station"
+
     id = Column(Integer, primary_key=True)
     name = Column(String)
     lat = Column(Float)
     lng = Column(Float)
-    connections = Column(Integer, ForeignKey("line.id"))
-    line = relationship("Line", back_populates="station")
+
+    line = relationship("Line", secondary=MetroMatch, back_populates="station")
+
 
 class Line(Base):
     __tablename__ = "line"
+
     id = Column(Integer, primary_key=True)
     name = Column(String)
     color = Column(String)
-    station = relationship("Station", back_populates="line")
+
+    station = relationship("Station", secondary=MetroMatch, back_populates="line")
