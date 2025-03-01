e4c330bbc93c96f3e6a6a058ffd991bdf3019724
Arisa-a-a <anastasiash200512@gmail.com>
2025-03-01 22:17:21 +0800

    feat: create trade model

--- src/trade/models.py
+++ src/trade/models.py
@@ -15,3 +15,14 @@ class Offer(Base):
 
     item1 = relationship("Item")
     trade = relationship("Trade")
+
+class Trade(Base):
+    __tablename__ = "trade"
+    id = Column(Integer, primary_key = True, index = True)
+    seller = Column(Integer, ForeignKey('user.username'))
+    buyer = Column(Integer, ForeignKey('user.username'))
+    status = Column(Boolean)
+    created_at = Column(TIMESTAMP, default=datetime.datetime.utcnow)
+    finished_at = Column(TIMESTAMP)
+    expires_at = Column(TIMESTAMP)
+    offer = Column(Integer, ForeignKey('offer.id'))
