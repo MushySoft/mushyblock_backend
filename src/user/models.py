608e5b5715fdf28bcb6818bfed0ae7a944a54cef
kk10 <kamy.zero.10@gmail.com>
2025-03-07 17:51:34 +0300

    chore: restruct User model

--- src/user/models.py
+++ src/user/models.py
@@ -10,13 +10,14 @@ class User(Base):
     __tablename__ = "user"
 
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String(12), unique=True, nullable=False)
-    email = Column(String(16), unique=True, nullable=False)
+    username = Column(String(16), unique=True, nullable=False)
+    email = Column(String(32), unique=True)
     hashed_password = Column(String(255), nullable=False)
-    name = Column(String(12), nullable=False)
+    name = Column(String(16), nullable=False)
     created_at = Column(TIMESTAMP, default=datetime.datetime.utcnow, nullable=False)
     last_login = Column(TIMESTAMP, nullable=False)
-    subscriptions = Column(Integer, ForeignKey("subscription.id"), nullable=False)
+    id_subscription = Column(Integer, ForeignKey("subscription.id"), nullable=False)
     balance = Column(Float, default=0.0, nullable=False)
+    avatar = Column(String(255))
 
     subscription = relationship("Subscription", back_populates="user")
