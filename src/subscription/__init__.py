3397fe2cf78abd2075ba531ef10750d33c587124
kk10 <kamy.zero.10@gmail.com>
2025-03-26 19:53:53 +0300

    chore(subscriptions): remove unused imports from __init__.py

--- src/subscription/__init__.py
+++ src/subscription/__init__.py
@@ -1,17 +1,14 @@
 from src.subscription.models import Subscription, SubscriptionType
-from src.subscription.schemas import SubscriptionTypeSchema, SubscriptionSchema, SubscriptionsResponse, SubscriptionPurchaseSchema, SubscriptionPurchaseRequest, ActiveSubscriptionSchema, ActiveSubscriptionsResponse
+from src.subscription.schemas import SubscriptionsResponse, SubscriptionPurchaseSchema, SubscriptionPurchaseRequest, ActiveSubscriptionsResponse
 from src.subscription.service import get_available_subscriptions, purchase_subscription, get_active_subscriptions
 from src.subscription.router import router
 
 __all__ = [
     "Subscription",
     "SubscriptionType",
-    "SubscriptionTypeSchema",
-    "SubscriptionSchema",
     "SubscriptionsResponse",
     "SubscriptionPurchaseSchema",
     "SubscriptionPurchaseRequest",
-    "ActiveSubscriptionSchema",
     "ActiveSubscriptionsResponse",
     "get_available_subscriptions",
     "purchase_subscription",
