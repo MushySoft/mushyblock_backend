dd908bf47d44a8342c709706be0cfb8d43231e80
Юрий Бурцев <yurtsev@Mac-mini-Urij.local>
2025-04-15 22:15:55 +0300

    feat(trade): add GET route for retrieving item and service offers

--- src/trade/router.py
+++ src/trade/router.py
@@ -3,8 +3,8 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.trade.schemas import OfferCreate
 from src.trade.service import create_offer_db
+from src.trade.service import get_offer_db
 from src import get_db
-
 
 router = APIRouter()
 
@@ -12,3 +12,8 @@ router = APIRouter()
 @router.post("/offer/create", description="создать офер типа item или service")
 async def create_offer(offer_data: OfferCreate, db: AsyncSession = Depends(get_db)):
     return await create_offer_db(db, offer_data)
+
+
+@router.get("/offers", description="получаем список всех существующих Item и service")
+async def get_offers(db: AsyncSession = Depends(get_db)):
+    return await get_offer_db(db)
